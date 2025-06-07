d13d353e0669058620c4ae37f8de99cea844afe3
keiralynng <92342273+keiralynng@users.noreply.github.com>
2025-06-07 13:02:47 -0400

    Create exercise1_2.c

---
+++ Chapter 1/exercise1_2.c
@@ -0,0 +1,12 @@
+/* exercise 1.2 */ 
+
+#include <stdio.h> 
+main()
+{
+printf("tab: \t\n"); 
+printf("backspace: \b\n"); 
+printf("double quote: \"\n"); 
+printf("single quote: \'\n"); 
+printf("backslash: \\\n"); 
+}  
+  
