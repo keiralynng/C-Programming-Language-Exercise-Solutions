643eb67b5102d1c2f374821bf62680d19fadbef9
keiralynng <92342273+keiralynng@users.noreply.github.com>
2025-06-07 13:06:09 -0400

    Create exercise1_3.c

---
+++ Chapter 1/exercise1_3.c
@@ -0,0 +1,21 @@
+/* exercise 1.3 solution */
+
+#include <stdio.h>
+main()
+{
+printf("TABLE FOR FAHR AND CELSIUS TEMPS\n");
+
+        float fahr, celsius;
+        int lower, upper, step;
+
+        lower = 0; /* lower limit of temp table */
+        upper = 300; /* upper */
+        step = 20; /* step size */
+
+        fahr = lower;
+        while (fahr <= upper){
+                celsius = (5.0/9.0) * (fahr-32.0);
+                printf("%3.0f %6.1f\n", fahr, celsius);
+                fahr = fahr + step;
+        }
+}
